0e1c8ec435f38c8a723309766d02015b0a4039e7
Uarsa <59147934+Uarsa@users.noreply.github.com>
2020-01-04 18:23:44 +0300

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -46,11 +46,9 @@ def send_welcome(message):
 def handle_text(message):
         if message.text == 'привет' or message.text == 'Привет':
                 bot.send_message(message.chat.id, 'И тебе привет, ' + message.from_user.first_name + '!' + '\nЯ умею показывать температуру за окном, курс валют и отвечать непониманием на твои сообщения)\nВыбери команду из меню или напиши мне что-нибудь.')
-        elif message.text == '\ud83d\udcb2':
-                bot.send_message(message.from_user.id, "RANDOM!!!!!")
         elif message.text == 'пока' or message.text == 'Пока':
                 bot.send_message(message.chat.id, 'Успехов в новом году!\nЗаходи ещё, позже у меня будет больше возможностей с:')       
-        elif message.text == '\u26c5\ufe0f':
+        elif message.text == '\ud83c\udfb2':
                 city = 'Sankt-Peterburg'
                 observation = owm.weather_at_place(city)
                 w = observation.get_weather()
