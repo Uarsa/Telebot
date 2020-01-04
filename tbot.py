38f8ebe4970a0a59f4fc4ed684759c988cc626e5
Uarsa <59147934+Uarsa@users.noreply.github.com>
2020-01-04 18:32:03 +0300

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -21,7 +21,7 @@ owm = pyowm.OWM('31c500a5d252323b3c085d510cacf4e4')
 def handle_start(message):
         user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
         #user_markup.row('/start', '\u26c5\ufe0f', '\ud83d\udcb2', '\ud83c\udfb2')
-        user_markup.row('/start', '\ud83d\udcb2', '\u2600\ufe0f', '\ud83c\udfb2')
+        user_markup.row('/start', '\u26c5\ufe0f', '\u2600\ufe0f', '\ud83c\udfb2')
         bot.send_message(message.from_user.id, "Давай начнём.\nНапиши мне \'привет\'", reply_markup=user_markup)
 
 
@@ -48,6 +48,11 @@ def handle_text(message):
                 bot.send_message(message.chat.id, 'И тебе привет, ' + message.from_user.first_name + '!' + '\nЯ умею показывать температуру за окном, курс валют и отвечать непониманием на твои сообщения)\nВыбери команду из меню или напиши мне что-нибудь.')
         elif message.text == 'пока' or message.text == 'Пока':
                 bot.send_message(message.chat.id, 'Успехов в новом году!\nЗаходи ещё, позже у меня будет больше возможностей с:')       
+        elif message.text == '\u26c5\ufe0f':
+                city = 'Sankt-Peterburg'
+                observation = owm.weather_at_place(city)
+                w = observation.get_weather()
+                bot.send_message(message.from_user.id, "В Санкт-Петербурге сейчас RANDOM!!!")
         elif message.text == '\u2600\ufe0f':
                 city = 'Sankt-Peterburg'
                 observation = owm.weather_at_place(city)
